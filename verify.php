81e71f22fbeeaecf1174201c5db63e93093f30b0
Ogatoz <gots12345@hotmail.com>
2017-03-07 16:25:37 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,3 +1,6 @@
+$proxy = 'velodrome.usefixie.com:80';
+$proxyauth = 'fixie:DVHb9WmDvqT8YCv';
+
 <?php
 $access_token = '5s3beWIw+vG+G1W0C4svPIUEN//gmACmJKSTVhI9N2ieM9bqMNYdVKY2bLWumtoofFFcHdU7c9L4bVlXZmZuqlF8H9RQQLwIHn3J+rrdwIqjY8b/LyBg98tLI5PpcICcmwYknf+LaxUVGTOTPfyphgdB04t89/1O/w1cDnyilFU=';
 
@@ -9,6 +12,8 @@ $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt($ch, CURLOPT_PROXY, $proxy);
+curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
 $result = curl_exec($ch);
 curl_close($ch);
 
