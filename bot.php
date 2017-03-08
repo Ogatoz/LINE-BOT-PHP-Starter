2c3c47ebc256e2bffdf48b8ae09828b8ec7f7f20
Ogatoz <gots12345@hotmail.com>
2017-03-08 18:36:04 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -32,7 +32,7 @@ if (!is_null($events['events']))
 			else
 			$messages = [
 				'type' => 'text',
-				'text' => $text
+				'text' => 'Invalid Input'
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
